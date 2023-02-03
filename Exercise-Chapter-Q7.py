90cce3424337aec58548ea92ed475dc4557f18a2
Jae Ung Jung <roosa0210@gmail.com>
2023-02-03 16:27:46 +0900

    Exercise-Chapter2-Q8

---
+++ Exercise-Chapter-Q7.py
@@ -0,0 +1,16 @@
+# Q7 Answer Template
+
+def solution(arr):
+    answer = [arr[0]]
+
+    for i in range(1,len(arr)):
+        if arr[i] != arr[i-1]:
+            answer.append(arr[i])
+  
+
+    return answer
+
+#arr = [1,1,3,3,0,0,1,1]
+arr = [4,4,4,3,3]
+answer = solution(arr)
+print(answer)
