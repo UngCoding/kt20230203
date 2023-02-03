b87e666e8554db61e2b18a85dffe4aed8f49b61f
Jae Ung Jung <roosa0210@gmail.com>
2023-02-03 15:19:47 +0900

    Exercise-Chapter2-Q3

---
+++ Exercise-Chapter2-Q3.py
@@ -0,0 +1,16 @@
+# Q3 answer template
+
+def solution(store, customer):
+    answer = []
+    for i in customer:
+        if i in store:
+            answer.append('yes')
+        else:
+            answer.append('no')
+    
+    return answer
+
+store = [1,2,3,7,8]
+customer = [1,5,8,4,6]
+answer = solution(store, customer)
+print(answer)
